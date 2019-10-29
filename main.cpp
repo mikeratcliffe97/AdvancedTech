c1259f6c37788f62b833945903ded97b6786c0d7
Mike Ratcliffe <michael2.ratcliffe@live.uwe.ac.uk>
2019-10-29 17:25:30 +0000

    Added cube manipulation

--- main.cpp
+++ main.cpp
@@ -1,218 +1,4 @@
-//Include and link appropriate libraries and headers//
-#pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "d3dx11.lib")
-#pragma comment(lib, "d3dx10.lib")
-#pragma comment (lib, "D3D10_1.lib")
-#pragma comment (lib, "DXGI.lib")
-#pragma comment (lib, "D2D1.lib")
-#pragma comment (lib, "dwrite.lib")
-///////////////**************new**************////////////////////
-#pragma comment (lib, "dinput8.lib")
-#pragma comment (lib, "dxguid.lib")
-///////////////**************new**************////////////////////
-
-#include <windows.h>
-#include <d3d11.h>
-#include <d3dx11.h>
-#include <d3dx10.h>
-#include <xnamath.h>
-#include <D3D10_1.h>
-#include <DXGI.h>
-#include <D2D1.h>
-#include <sstream>
-#include <dwrite.h>
-///////////////**************new**************////////////////////
-#include <dinput.h>
-///////////////**************new**************////////////////////
-
-//Global Declarations - Interfaces//
-IDXGISwapChain* SwapChain;
-ID3D11Device* d3d11Device;
-ID3D11DeviceContext* d3d11DevCon;
-ID3D11RenderTargetView* renderTargetView;
-ID3D11Buffer* squareIndexBuffer;
-ID3D11DepthStencilView* depthStencilView;
-ID3D11Texture2D* depthStencilBuffer;
-ID3D11Buffer* squareVertBuffer;
-ID3D11VertexShader* VS;
-ID3D11PixelShader* PS;
-ID3D11PixelShader* D2D_PS;
-ID3D10Blob* VS_Buffer;
-ID3D10Blob* PS_Buffer;
-ID3D10Blob* D2D_PS_Buffer;
-ID3D11InputLayout* vertLayout;
-ID3D11Buffer* cbPerObjectBuffer;
-ID3D11BlendState* Transparency;
-ID3D11RasterizerState* CCWcullMode;
-ID3D11RasterizerState* CWcullMode;
-ID3D11ShaderResourceView* CubesTexture;
-ID3D11SamplerState* CubesTexSamplerState;
-ID3D11Buffer* cbPerFrameBuffer;
-
-ID3D10Device1* d3d101Device;
-IDXGIKeyedMutex* keyedMutex11;
-IDXGIKeyedMutex* keyedMutex10;
-ID2D1RenderTarget* D2DRenderTarget;
-ID2D1SolidColorBrush* Brush;
-ID3D11Texture2D* BackBuffer11;
-ID3D11Texture2D* sharedTex11;
-ID3D11Buffer* d2dVertBuffer;
-ID3D11Buffer* d2dIndexBuffer;
-ID3D11ShaderResourceView* d2dTexture;
-IDWriteFactory* DWriteFactory;
-IDWriteTextFormat* TextFormat;
-
-///////////////**************new**************////////////////////
-IDirectInputDevice8* DIKeyboard;
-IDirectInputDevice8* DIMouse;
-///////////////**************new**************////////////////////
-
-std::wstring printText;
-
-//Global Declarations - Others//
-LPCTSTR WndClassName = "AdvancedTech";
-HWND hwnd = NULL;
-HRESULT hr;
-
-const int Width = 900;
-const int Height = 900;
-
-///////////////**************new**************////////////////////
-DIMOUSESTATE mouseLastState;
-LPDIRECTINPUT8 DirectInput;
-
-float rotx = 0;
-float rotz = 0;
-float scaleX = 1.0f;
-float scaleY = 1.0f;
-
-XMMATRIX Rotationx;
-XMMATRIX Rotationz;
-///////////////**************new**************////////////////////\\
-
-XMMATRIX WVP;
-XMMATRIX cube1World;
-XMMATRIX cube2World;
-XMMATRIX camView;
-XMMATRIX camProjection;
-
-XMMATRIX d2dWorld;
-
-XMVECTOR camPosition;
-XMVECTOR camTarget;
-XMVECTOR camUp;
-
-XMVECTOR DefaultForward = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
-XMVECTOR DefaultRight = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
-XMVECTOR camForward = XMVectorSet(0.0f, 0.0f, 1.0f, 0.0f);
-XMVECTOR camRight = XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f);
-
-XMMATRIX camRotationMatrix;
-XMMATRIX groundWorld;
-
-float moveLeftRight = 0.0f;
-float moveBackForward = 0.0f;
-
-float camYaw = 0.0f;
-float camPitch = 0.0f;
-
-XMMATRIX Rotation;
-XMMATRIX Scale;
-XMMATRIX Translation;
-float rot = 0.01f;
-
-double countsPerSecond = 0.0;
-__int64 CounterStart = 0;
-
-int frameCount = 0;
-int fps = 0;
-
-__int64 frameTimeOld = 0;
-double frameTime;
-
-//Function Prototypes//
-bool InitializeDirect3d11App(HINSTANCE hInstance);
-void CleanUp();
-bool InitScene();
-void DrawScene();
-bool InitD2D_D3D101_DWrite(IDXGIAdapter1* Adapter);
-void InitD2DScreenTexture();
-void UpdateScene(double time);
-void UpdateCamera();
-void RenderText(std::wstring text, int inInt);
-
-void StartTimer();
-double GetTime();
-double GetFrameTime();
-
-bool InitializeWindow(HINSTANCE hInstance,
-	int ShowWnd,
-	int width, int height,
-	bool windowed);
-int messageloop();
-
-///////////////**************new**************////////////////////
-bool InitDirectInput(HINSTANCE hInstance);
-void DetectInput(double time);
-///////////////**************new**************////////////////////
-
-LRESULT CALLBACK WndProc(HWND hWnd,
-	UINT msg,
-	WPARAM wParam,
-	LPARAM lParam);
-
-//Create effects constant buffer's structure//
-struct cbPerObject
-{
-	XMMATRIX  WVP;
-	XMMATRIX  World;
-};
-
-cbPerObject cbPerObj;
-
-struct Light
-{
-	Light()
-	{
-		ZeroMemory(this, sizeof(Light));
-	}
-
-	XMFLOAT3 dir;
-	float pad;
-	XMFLOAT4 ambient;
-	XMFLOAT4 diffuse;
-};
-
-Light light;
-
-struct cbPerFrame
-{
-	Light  light;
-};
-
-cbPerFrame constbuffPerFrame;
-
-//Vertex Structure and Vertex Layout (Input Layout)//
-struct Vertex    //Overloaded Vertex Structure
-{
-	Vertex() {}
-	Vertex(float x, float y, float z,
-		float u, float v,
-		float nx, float ny, float nz)
-		: pos(x, y, z), texCoord(u, v), normal(nx, ny, nz) {}
-
-	XMFLOAT3 pos;
-	XMFLOAT2 texCoord;
-	XMFLOAT3 normal;
-};
-
-D3D11_INPUT_ELEMENT_DESC layout[] =
-{
-	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	{ "NORMAL",     0, DXGI_FORMAT_R32G32B32_FLOAT,    0, 20, D3D11_INPUT_PER_VERTEX_DATA, 0}
-};
-UINT numElements = ARRAYSIZE(layout);
+#include "main.h"
 
 int WINAPI WinMain(HINSTANCE hInstance,    //Main windows function
 	HINSTANCE hPrevInstance,
@@ -513,6 +299,7 @@ bool InitDirectInput(HINSTANCE hInstance)
 
 void DetectInput(double time)
 {
+	
 	DIMOUSESTATE mouseCurrState;
 
 	BYTE keyboardState[256];
@@ -530,22 +317,94 @@ void DetectInput(double time)
 
 	if (keyboardState[DIK_ESCAPE] & 0x80)
 		PostMessage(hwnd, WM_DESTROY, 0, 0);
+	switch (placementmode)
+	{
+		case 0:
+	{
+		if (keyboardState[DIK_LEFT] & 0x80)
+		{
+			tranX -= 4.0f * time;
+			
+		}
+		if (keyboardState[DIK_RIGHT] & 0x80)
+		{
+			tranX += 4.0f * time;
+			
+		}
+		if (keyboardState[DIK_UP] & 0x80)
+		{
+			tranY += 4.0f * time;
+			
+		}
+		if (keyboardState[DIK_DOWN] & 0x80)
+		{
+			tranY -= 4.0f * time;
+		
+		}
 
-	if (keyboardState[DIK_LEFT] & 0x80)
-	{
-		scaleX -= 1.0f * time;
+		if (keyboardState[DIK_TAB] & 0x80)
+		{
+			placementmode = 1;
+			break;
+		
+		}
+		break;
 	}
-	if (keyboardState[DIK_RIGHT] & 0x80)
+
+	case 1:
 	{
-		scaleX += 1.0f * time;
+		if (keyboardState[DIK_LEFT] & 0x80)
+		{
+			rotx -= 1.0f * time;
+		}
+		if (keyboardState[DIK_RIGHT] & 0x80)
+		{
+			rotx += 1.0f * time;
+		}
+		if (keyboardState[DIK_UP] & 0x80)
+		{
+			rotz += 1.0f * time;
+		}
+		if (keyboardState[DIK_DOWN] & 0x80)
+		{
+			rotz -= 1.0f * time;
+		}
+
+		if (keyboardState[DIK_TAB] & 0x80)
+		{
+			placementmode = 2;
+			break;
+		}
+		break;
 	}
-	if (keyboardState[DIK_UP] & 0x80)
+
+	case 2:
 	{
-		scaleY += 1.0f * time;
+		if (keyboardState[DIK_LEFT] & 0x80)
+		{
+			scaleX -= 1.0f * time;
+		}
+		if (keyboardState[DIK_RIGHT] & 0x80)
+		{
+			scaleX += 1.0f * time;
+		}
+		if (keyboardState[DIK_UP] & 0x80)
+		{
+			scaleY += 1.0f * time;
+		}
+		if (keyboardState[DIK_DOWN] & 0x80)
+		{
+			scaleY -= 1.0f * time;
+		}
+
+		if (keyboardState[DIK_TAB] & 0x80)
+		{
+			placementmode = 0;
+			break;
+		}
+		break;
 	}
-	if (keyboardState[DIK_DOWN] & 0x80)
-	{
-		scaleY -= 1.0f * time;\
+	
 	}
 	//if (mouseCurrState.lX != mouseLastState.lX)
 	//{
@@ -659,6 +518,8 @@ void InitD2DScreenTexture()
 		Vertex(-1.0f,  1.0f, -1.0f, 0.0f, 0.0f,-1.0f,  1.0f, -1.0f),
 		Vertex(1.0f,  1.0f, -1.0f, 1.0f, 0.0f, 1.0f,  1.0f, -1.0f),
 		Vertex(1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f, -1.0f),
+	
+
 	};
 
 	DWORD indices[] = {
@@ -732,7 +593,7 @@ bool InitScene()
 		Vertex(-1.0f,  1.0f, -1.0f, 0.0f, 0.0f,-1.0f,  1.0f, -1.0f),
 		Vertex(1.0f,  1.0f, -1.0f, 1.0f, 0.0f, 1.0f,  1.0f, -1.0f),
 		Vertex(1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f, -1.0f),
-
+		
 		// Back Face
 		Vertex(-1.0f, -1.0f, 1.0f, 1.0f, 1.0f,-1.0f, -1.0f, 1.0f),
 		Vertex(1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, -1.0f, 1.0f),
@@ -983,12 +844,12 @@ void UpdateScene(double time)
 	Rotation = XMMatrixRotationAxis(rotyaxis, rot);
 	Rotationx = XMMatrixRotationAxis(rotxaxis, rotx);
 	Rotationz = XMMatrixRotationAxis(rotzaxis, rotz);
-	Translation = XMMatrixTranslation(0.0f, 0.0f, -4.0f);
+	Translation = XMMatrixTranslation(tranX, tranY, tranZ);
 
 	//Set cube1's world space using the transformations
-	cube1World = Translation * Rotation * Rotationx * Rotationz;
+	cube1World = Translation * Rotation * Rotationx * Rotationz * Scale;
 	///////////////**************new**************////////////////////
-
+	Scale = XMMatrixScaling(scaleX, scaleY, 1.3f);
 	//Reset cube2World
 	cube2World = XMMatrixIdentity();
 
@@ -1081,7 +942,7 @@ void RenderText(std::wstring text, int inInt)
 void DrawScene()
 {
 	//Clear our render target and depth/stencil view
-	float bgColor[4] = { (0.0f, 0.0f, 0.0f, 3.0f) };
+	float bgColor[4] = { (0.1f, 0.1f, 0.1f, 0.1f) };
 	d3d11DevCon->ClearRenderTargetView(renderTargetView, bgColor);
 	d3d11DevCon->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 
